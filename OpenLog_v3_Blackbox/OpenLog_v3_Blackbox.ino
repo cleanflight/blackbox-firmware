f45d09c8bacd5b1b01fcb96627f30d967a0d7979
Nicholas Sherlock <n.sherlock@gmail.com>
2015-03-06 19:26:33 +1300

    Add back CONFIG.TXT but only keep the baud rate setting, default 115200

--- OpenLog_v3_Blackbox/OpenLog_v3_Blackbox.ino
+++ OpenLog_v3_Blackbox/OpenLog_v3_Blackbox.ino
@@ -18,6 +18,8 @@
  OpenLog automatically works with 512MB, 1GB, 2GB, 4GB, 8GB, and 16GB microSD cards. We recommend FAT16 for 2GB and smaller cards. We
  recommend FAT32 for 4GB and larger cards.
  
+ OpenLog will run at 115200 baud by default.
+ 
  During power up, you will see '12<'. '1' indicates the serial connection is established. '2' indicates
  the SD card has been successfully initialized. '<' indicates OpenLog is ready to receive serial characters.
  
@@ -47,7 +49,7 @@
  v3 Blackbox edition
  
  This version has been modified for use with the Blackbox feature for Baseflight / Cleanflight. This version 
- discards the CONFIG.TXT system and instead hard-codes the correct settings (115200 baud).
+ removes useless settings from CONFIG.TXT and changes the default baud rate to 115200.
  
  */
 
@@ -69,22 +71,21 @@ SerialPort<0, 800, 0> NewSerial;
 //#define DEBUG  1
 #define DEBUG  0
 
-//The bigger the receive buffer, the less likely we are to drop characters at high speed. However, the ATmega has a limited amount of
-//RAM. This debug mode allows us to view available RAM at various stages of the program
-//#define RAM_TESTING  1 //On
-#define RAM_TESTING  0 //Off
+#define CFG_FILENAME "config.txt" //This is the name of the file that contains the unit settings
 
-#define SEQ_FILENAME "SEQLOG00.TXT" //This is the name for the file when you're in sequential modeu
+#define MAX_CFG "1000000\0" // baud 
+#define CFG_LENGTH (strlen(MAX_CFG) + 1) //Length of text found in config file. strlen ignores \0 so we have to add it back 
 
 //Internal EEPROM locations for the user settings
 #define LOCATION_FILE_NUMBER_LSB	0x03
 #define LOCATION_FILE_NUMBER_MSB	0x04
+#define LOCATION_BAUD_SETTING_HIGH	0x09
+#define LOCATION_BAUD_SETTING_MID	0x0A
+#define LOCATION_BAUD_SETTING_LOW	0x0B
 
 #define BAUD_MIN  300
 #define BAUD_MAX  1000000
-
-#define MODE_NEWLOG	0
-#define MODE_SEQLOG     1
+#define BAUD_DEFAULT 115200
 
 //STAT1 is a general LED and indicates serial traffic
 #define STAT1  5 //On PORTD
@@ -109,8 +110,7 @@ Sd2Card card;
 SdVolume volume;
 SdFile currentDirectory;
 
-long setting_uart_speed; //This is the baud rate that the system runs at, default is 9600. Can be 300 to 1,000,000
-byte setting_system_mode; //This is the mode the system runs in, default is MODE_NEWLOG
+long setting_uart_speed; //This is the baud rate that the system runs at. Can be 300 to 1,000,000
 
 //Passes back the available amount of free RAM
 int freeRam() {
@@ -171,8 +171,8 @@ void setup(void) {
     power_timer2_disable();
     power_adc_disable();
 
-    init_system_settings();
-
+    read_system_settings();
+    
     //Setup UART
     NewSerial.begin(setting_uart_speed);
     if (setting_uart_speed < 500)      // check for slow baud rates
@@ -197,20 +197,15 @@ void setup(void) {
     NewSerial.print(F("2"));
 
     printRam(); //Print the available RAM
+    
+    read_config_file();
 }
 
 void loop(void) {
-    //If we are in new log mode, find a new file name to write to
-    if (setting_system_mode == MODE_NEWLOG) {
-        //new_file_name = newlog();
-        append_file (newlog()); //Append the file name that newlog() returns
+    append_file(newlog());
+    
+    while(1); //We should never get this far
 }
-        //If we are in sequential log mode, determine if seqlog.txt has been created or not, and then open it for logging
-        if(setting_system_mode == MODE_SEQLOG)
-        seqlog();
-
-        while(1);//We should never get this far
-    }
 
 //Log to a new file everytime the system boots
 //Checks the spots in EEPROM for the next available LOG# file name
@@ -285,30 +280,6 @@ char* newlog(void) {
     return (new_file_name);
 }
 
-//Log to the same file every time the system boots, sequentially
-//Checks to see if the file SEQLOG.txt is available
-//If not, create it
-//If yes, append to it
-//Return 0 on error
-//Return anything else on sucess
-void seqlog(void) {
-    SdFile seqFile;
-
-    char sequentialFileName[strlen(SEQ_FILENAME)]; //Limited to 8.3
-    strcpy_P(sequentialFileName, PSTR(SEQ_FILENAME)); //This is the name of the config file. 'config.sys' is probably a bad idea.
-
-    //Try to create sequential file
-    if (!seqFile.open(&currentDirectory, sequentialFileName,
-            O_CREAT | O_WRITE)) {
-        NewSerial.print(F("Error creating SEQLOG\n"));
-        return;
-    }
-
-    seqFile.close(); //Close this new file we just opened
-
-    append_file(sequentialFileName);
-}
-
 //This is the most important function of the device. These loops have been tweaked as much as possible.
 //Modifying this loop may negatively affect how well the device can record at high baud rates.
 //Appends a stream of serial data to a given file
@@ -350,7 +321,7 @@ void append_file(char* file_name) {
             STAT1_PORT ^= (1 << STAT1); //Toggle the STAT1 LED each time we record the buffer
 
             //This will force a sync approximately every 5 seconds
-            if ((millis() - lastSyncTime) > MAX_TIME_BEFORE_SYNC_MSEC) { 
+            if ((millis() - lastSyncTime) > MAX_TIME_BEFORE_SYNC_MSEC) {
                 //This is here to make sure a log is recorded in the instance
                 //where the user is throwing non-stop data at the unit from power on to forever
                 workingFile.sync(); //Sync the card
@@ -392,8 +363,229 @@ void blink_error(byte ERROR_TYPE) {
     }
 }
 
-//Set up system settings
-void init_system_settings(void) {
-    setting_uart_speed = 115200;
-    setting_system_mode = MODE_NEWLOG;
+//Resets all the system settings to safe values
+void set_default_settings(void) {
+    writeBaud(BAUD_DEFAULT);
+
+    //These settings are not recorded to the config file
+    //We can't do it here because we are not sure the FAT system is init'd
+}
+
+//Reads the current system settings from EEPROM
+//If anything looks weird, reset setting to default value
+void read_system_settings(void) {
+    //Read what the current UART speed is from EEPROM memory
+    setting_uart_speed = readBaud(); //Combine the three bytes
+    if (setting_uart_speed < BAUD_MIN || setting_uart_speed > BAUD_MAX) {
+        setting_uart_speed = BAUD_DEFAULT; //Reset if there is no speed stored
+        writeBaud(setting_uart_speed); //Record to EEPROM
+    }
+}
+
+void read_config_file(void) {
+    SdFile rootDirectory;
+    SdFile configFile;
+    if (!rootDirectory.openRoot(&volume))
+        systemError(ERROR_ROOT_INIT); // open the root directory
+
+    char configFileName[strlen(CFG_FILENAME)]; //Limited to 8.3
+    strcpy_P(configFileName, PSTR(CFG_FILENAME)); //This is the name of the config file. 'config.sys' is probably a bad idea.
+
+    //Check to see if we have a config file
+    if (!configFile.open(&rootDirectory, configFileName, O_READ)) {
+        //If we don't have a config file already, then create config file and record the current system settings to the file
+#if DEBUG
+        NewSerial.println(F("No config found - creating default:"));
+#endif
+        configFile.close();
+        rootDirectory.close(); //Close out the file system before we open a new one
+
+        //Record the current eeprom settings to the config file
+        record_config_file();
+        return;
+    }
+
+    //If we found the config file then load settings from file and push them into EEPROM
+#if DEBUG
+    NewSerial.println(F("Found config file!"));
+#endif
+
+    //Read up to 20 characters from the file. There may be a better way of doing this...
+    char c;
+    int len;
+    byte settings_string[CFG_LENGTH]; //"115200\0" = 115200 bps
+    for (len = 0; len < CFG_LENGTH; len++) {
+        if ((c = configFile.read()) < 0)
+            break; //We've reached the end of the file
+        if (c == '\0')
+            break; //Bail if we hit the end of this string
+        settings_string[len] = c;
+    }
+    configFile.close();
+    rootDirectory.close();
+
+#if DEBUG
+    //Print line for debugging
+    NewSerial.print(F("Text Settings: "));
+    for(int i = 0; i < len; i++)
+    NewSerial.write(settings_string[i]);
+    NewSerial.println();
+    NewSerial.print(F("Len: "));
+    NewSerial.println(len);
+#endif
+
+    //Default the system settings in case things go horribly wrong
+    long new_system_baud = BAUD_DEFAULT;
+
+    //Parse the settings out
+    byte i = 0, j = 0, setting_number = 0;
+    char new_setting[8]; //Max length of a setting is 6, the bps setting = '115200' plus '\0'
+    byte new_setting_int = 0;
+
+    for (i = 0; i < len; i++) {
+        //Pick out one setting from the line of text
+        for (j = 0; settings_string[i] != ',' && i < len && j < 6;) {
+            new_setting[j] = settings_string[i];
+            i++;
+            j++;
+        }
+
+        new_setting[j] = '\0'; //Terminate the string for array compare
+        new_setting_int = atoi(new_setting); //Convert string to int
+
+        switch (setting_number) {
+            case 0: //Baud rate
+                new_system_baud = strtolong(new_setting);
+
+                //Basic error checking
+                if (new_system_baud < BAUD_MIN || new_system_baud > BAUD_MAX)
+                    new_system_baud = BAUD_DEFAULT;
+            break;
+            default:
+                //We're done!
+            break;
+        }
+        
+        setting_number++;
+    }
+
+    //We now have the settings loaded into the global variables. Now check if they're different from EEPROM settings
+    boolean recordNewSettings = false;
+
+    if (new_system_baud != setting_uart_speed) {
+        //If the baud rate from the file is different from the current setting,
+        //Then update the setting to the file setting
+        //And re-init the UART
+        writeBaud(new_system_baud); //Write this baudrate to EEPROM
+        setting_uart_speed = new_system_baud;
+        NewSerial.begin(setting_uart_speed); //Move system to new uart speed
+
+        recordNewSettings = true;
+    }
+
+    //We don't want to constantly record a new config file on each power on. Only record when there is a change.
+    if (recordNewSettings == true)
+        record_config_file(); //If we corrected some values because the config file was corrupt, then overwrite any corruption
+#if DEBUG
+        else
+        NewSerial.println(F("Config file matches system settings"));
+#endif
+
+    //All done! New settings are loaded. System will now operate off new config settings found in file.
+}
+
+//Records the current EEPROM settings to the config file
+//If a config file exists, it is trashed and a new one is created
+void record_config_file(void) {
+    //I'm worried that the user may not be in the root directory when modifying config settings. If that is the case,
+    //config file will not be found and it will be created in some erroneus directory. The changes to user settings may be lost on the
+    //next power cycles. To prevent this, we will open another instance of the file system, then close it down when we are done.
+    SdFile rootDirectory;
+    SdFile myFile;
+    if (!rootDirectory.openRoot(&volume))
+        systemError(ERROR_ROOT_INIT); // open the root directory
+
+    char configFileName[strlen(CFG_FILENAME)];
+    strcpy_P(configFileName, PSTR(CFG_FILENAME)); //This is the name of the config file. 'config.sys' is probably a bad idea.
+
+    //If there is currently a config file, trash it
+    if (myFile.open(&rootDirectory, configFileName, O_WRITE)) {
+        if (!myFile.remove()) {
+            NewSerial.println(F("Remove config failed"));
+            myFile.close(); //Close this file
+            rootDirectory.close(); //Close this file structure instance
+            return;
+        }
+    }
+
+    //myFile.close(); //Not sure if we need to close the file before we try to reopen it
+
+    //Create config file
+    if (myFile.open(&rootDirectory, configFileName,
+            O_CREAT | O_APPEND | O_WRITE) == 0) {
+        NewSerial.println(F("Create config failed"));
+        myFile.close(); //Close this file
+        rootDirectory.close(); //Close this file structure instance
+        return;
+    }
+    //Config was successfully created, now record current system settings to the config file
+
+    char settings_string[CFG_LENGTH]; //"115200,103,214,0,1,1\0" = 115200 bps, escape char of ASCII(103), 214 times, new log mode, verbose on, echo on.
+
+    //Before we read the EEPROM values, they've already been tested and defaulted in the read_system_settings function
+    long current_system_baud = readBaud();
+
+    //Convert system settings to visible ASCII characters
+    sprintf_P(settings_string, PSTR("%ld\0"), current_system_baud);
+
+    //Record current system settings to the config file
+    if (myFile.write(settings_string, strlen(settings_string))
+            != strlen(settings_string))
+        NewSerial.println(F("error writing to file"));
+
+    myFile.println(); //Add a break between lines
+
+    //Add a decoder line to the file
+#define HELP_STR "baud\0"
+    char helperString[strlen(HELP_STR) + 1]; //strlen is preprocessed but returns one less because it ignores the \0
+    strcpy_P(helperString, PSTR(HELP_STR));
+    myFile.write(helperString); //Add this string to the file
+
+    myFile.sync(); //Sync all newly written data to card
+    myFile.close(); //Close this file
+    rootDirectory.close(); //Close this file structure instance
+    //Now that the new config file has the current system settings, nothing else to do!
+}
+
+//Given a baud rate (long number = four bytes but we only use three), record to EEPROM
+void writeBaud(long uartRate) {
+    EEPROM.write(LOCATION_BAUD_SETTING_HIGH,
+            (byte)((uartRate & 0x00FF0000) >> 16));
+    EEPROM.write(LOCATION_BAUD_SETTING_MID, (byte)(uartRate >> 8));
+    EEPROM.write(LOCATION_BAUD_SETTING_LOW, (byte) uartRate);
+}
+
+//Look up the baud rate. This requires three bytes be combined into one long
+long readBaud(void) {
+    byte uartSpeedHigh = EEPROM.read(LOCATION_BAUD_SETTING_HIGH);
+    byte uartSpeedMid = EEPROM.read(LOCATION_BAUD_SETTING_MID);
+    byte uartSpeedLow = EEPROM.read(LOCATION_BAUD_SETTING_LOW);
+
+    long uartSpeed = ((long) uartSpeedHigh << 16) | ((long) uartSpeedMid << 8) | uartSpeedLow;
+
+    return (uartSpeed);
+}
+
+//End core system functions
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//A rudimentary way to convert a string to a long 32 bit integer
+//Used by the read command, in command shell and baud from the system menu
+uint32_t strtolong(const char* str)
+{
+  uint32_t l = 0;
+  while(*str >= '0' && *str <= '9')
+    l = l * 10 + (*str++ - '0');
+
+  return l;
 }
